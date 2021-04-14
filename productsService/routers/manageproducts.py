5d0e7991019bfb0fdeaa62864497752a40bbfd71
gnouf1 <jeuxforum26@gmail.com>
2021-04-14 18:12:05 +0200

    misc improve -> :+1:

--- productsService/routers/manageproducts.py
+++ productsService/routers/manageproducts.py
@@ -31,13 +31,18 @@ def manage_products(items: List[Item]):
     resDB = session.query(Products).filter(Products.pid.in_([i.id for i in items]))
     pr = resDB.all()
     if pr and len(pr) == len(items): # Id existante
+        listRet = []
         for item in items:
             currentProduct = session.query(Products).filter(Products.pid == item.id)
-
+            itemRet = {"id": item.id}
             if item.discPer is not None:
                 if item.discPer <= 100 and item.discPer >= 0:
-                    newDiscountPrice = currentProduct.first().price * (item.discPer*0.01)
+                    r = requests.get(url=ADRESS_CANVA + "tig/product/{}".format(item.id))
+                    remoteProduct = r.json()
+                    newDiscountPrice = remoteProduct["price"] * (item.discPer*0.01)
                     currentProduct.update({Products.discountPercentage: item.discPer, Products.discount: newDiscountPrice})
+                    itemRet["discountPercentage"] = item.discPer
+                    itemRet["discount"] = newDiscountPrice
                 else:
                     raise HTTPException(status_code=422, detail="On id {} product percentage is <0 or >100".format(item.id))
 
@@ -49,6 +54,10 @@ def manage_products(items: List[Item]):
                     newStock = 0
                     flagAv = False
                 currentProduct.update({Products.quantityInStock: newStock, Products.avaible: flagAv})
+                itemRet["quantityInStock"] = newStock
+                itemRet["availability"] = flagAv
+            listRet.append(itemRet)
         session.commit()
+        return {"status":"success", "New state":listRet}
     else:
         raise HTTPException(status_code=404, detail="At least one id doesn't match anything")
